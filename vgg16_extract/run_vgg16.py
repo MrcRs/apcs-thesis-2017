10d669751cbeec4d71d428c75e086fd298aec94c
ddtung <ddtung@apcs.vn>
2017-05-13 14:03:24 +0700

    push for sure

--- vgg16_extract/run_vgg16.py
+++ vgg16_extract/run_vgg16.py
@@ -7,7 +7,7 @@ import cv2
 import vgg16
 import config
 
-sample_paths = glob.glob(config.DATA_DIR + '/aia/aia_anger/*.png')
+sample_paths = glob.glob(config.DATA_DIR + '/bonnie/bonnie_surprise/*.png')
 sample_paths.sort()
 f = open('samples_paths', 'w')
 f.writelines(sample_paths)
@@ -24,19 +24,20 @@ with tf.Session() as sess:
 
 	for path in sample_paths:
 		img = cv2.imread(path)
-		img = cv2.resize(img, (224, 224))
-		img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-		img = img.reshape(1, 224, 224, 3)
+		if img != None:
+			img = cv2.resize(img, (224, 224))
+			img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+			img = img.reshape(1, 224, 224, 3)
 
-		feat = sess.run(vgg.relu6, feed_dict={images: img}) # extract feats from ReLU-6 layer.
+			feat = sess.run(vgg.relu6, feed_dict={images: img}) # extract feats from ReLU-6 layer.
 
-		dest_file = path.replace(config.DATA_DIR, config.OUT_DIR)
-		parent_fd = os.path.dirname(dest_file)
-		if not os.path.exists(parent_fd):
-			os.makedirs(parent_fd)
-		np.save(dest_file, feat)
+			dest_file = path.replace(config.DATA_DIR, config.OUT_DIR)
+			parent_fd = os.path.dirname(dest_file)
+			if not os.path.exists(parent_fd):
+				os.makedirs(parent_fd)
+			np.save(dest_file, feat)
 
-		count += 1
-		flog.write('%06d\t%s\n'%(count, path))
-		print (count, '\t', path)
-		# print ('%06d\t%s', %(count, path))
+			count += 1
+			flog.write('%06d\t%s\n'%(count, path))
+			print (count, '\t', path)
+			# print ('%06d\t%s', %(count, path))
